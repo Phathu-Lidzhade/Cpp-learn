9509a4c434db6c778e848f6031fe017cc6d5661d
Phathu <maddangerchris@gmail.com>
2026-09-19 19:27:44 +0200

    finished variables tutorial

--- helloworld.cpp
+++ helloworld.cpp
@@ -15,5 +15,43 @@ int main() {
 
   // Variables
 
+  //Integers
+  int x;
+  x = 10;
+
+  int y = 5;
+  int sum = x + y;
+
+  //Characters
+  char grade = 'A';
+  char initial = 'P';
+
+  //Booleans (true or false)
+  bool student = false;
+  bool power = true;
+
+  //Strings
+  std::string name = "Phathu";
+
+  //Const (a variable that does change)
+  const int LIGHT_SPEED = 299792458;
+  const double PI = 3.14159;
+  double radius = 10;
+  double circumference = 2 * PI * radius;
+
+
+
+  std::cout << x << '\n';
+  std::cout << y << '\n';
+  std::cout << sum << '\n';
+
+  std::cout << initial << '\n';
+
+  std::cout << name << '\n';
+  std::cout << "Hello " << name << '\n';
+  std::cout << "The sum is " << sum << " very exciting!" << '\n';
+
+  std::cout << circumference << '\n';
+
   return 0;
 }
