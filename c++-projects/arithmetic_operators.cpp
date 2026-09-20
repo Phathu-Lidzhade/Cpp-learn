ba320dbc943400836ab9aaad263cb980f0c5056c
Phathu <maddangerchris@gmail.com>
2026-09-20 09:30:49 +0200

    yes

---
+++ c++-projects/arithmetic_operators.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main(){
+
+  // Arithmetic Operators returns the results of (+ - * /)
+
+  int students = 20;
+  // Addition
+  //students = students + 1;
+  //students += 1;
+  //students++;
+
+  // Subtraction
+  //students = students - 1;
+  //students -= 1;
+  //students--;
+
+  // Multiplication
+  //students = students * 2;
+  //students *= 2;
+
+  // Division
+  //students = students / 3;
+  students /= 3;
+
+
+  std::cout << students;
+
+  return 0;
+}
