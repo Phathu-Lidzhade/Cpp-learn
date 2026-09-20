414a4d885817ccee7fa48ec6334e84b7d28ab34c
Phathu <maddangerchris@gmail.com>
2026-09-20 19:41:02 +0200

    Created a calculator app

---
+++ c++-projects/practice/calculator.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main() {
+
+  char mathOperator;
+  double num1;
+  double num2;
+  double result;
+
+  std::cout << "***********************CALCULATOR*************************\n";
+  std::cout << "\nEnter an operator (+ - * /): ";
+  std::cin >> mathOperator;
+
+  std::cout << "\nEnter first number: ";
+  std::cin >> num1;
+
+  std::cout << "\nEnter second number: ";
+  std::cin >> num2;
+
+  switch (mathOperator)
+  {
+  case '+':
+    result = num1 + num2;
+    std::cout << "Result: " << result;
+    break;
+
+  case '-':
+    result = num1 - num2;
+    std::cout << "Result: " << result;
+    break;
+
+  case '*':
+    result = num1 * num2;
+    std::cout << "Result: " << result;
+    break;
+
+  case '/':
+    result = num1 / num2;
+    std::cout << "Result: " << result;
+    break;
+  
+  default:
+    std::cout << "Enter a valid operator.";
+    break;
+  }
+
+  return 0;
+}
