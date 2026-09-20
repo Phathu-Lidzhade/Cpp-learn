02d942508e671dce464cb5d7f2f131edd82c07b0
Phathu <maddangerchris@gmail.com>
2026-09-20 10:45:40 +0200

    operators and conversions

--- c++-projects/arithmetic_operators.cpp
+++ c++-projects/arithmetic_operators.cpp
@@ -21,10 +21,33 @@ int main(){
 
   // Division
   //students = students / 3;
-  students /= 3;
+  //students /= 3;
+
+  // Modulus
+  //int remainder = students % 3;
+
+  //students = 6 - 5 + 4 * 3 / 2;
 
 
-  std::cout << students;
+  //std::cout << students;
+
+  // type conversion = changing a value of one data type to another data type
+  //                   Implicit = automatic
+  //                   Explicit = Precede value with new data type eg (int) x
+
+  //int x = 3.14;
+  //double x = (int) 3.14;
+
+  //std::cout << x;
+
+  //char x = 100;
+  //std::cout << (char)100;
+
+  int correct = 8;
+  int questions = 10;
+  double score = correct/(double)questions * 100;
+
+  std::cout << score << "%";
 
   return 0;
 }
